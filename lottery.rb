cca224ff9c26d12a670f21c1d70084e91cf42751
Brian Lewis <blewis8958@gmail.com>
2017-01-31 23:26:10 -0500

    Finished HW with 3 working functions for winning numbers, one off numbers, and losing numbers

--- lottery.rb
+++ lottery.rb
@@ -46,7 +46,8 @@ def winning__numbers_array(winning_array,participants_array)
 end
 
 def one_off_array(winning_array,participants_array)
-	final_array = Array.new
+	final_array2 = Array.new
+	one_off = Array.new
 
 	if winning_array.class != Array
 		return "Error, 1st entry must be an Array of numbers"
@@ -80,12 +81,113 @@ def one_off_array(winning_array,participants_array)
 
 		end
 
+		for i in 0..(winning_array.length - 1)
+			for j in 0..3
+				one_off.push(winning_array[i] - 10**j)
+				one_off.push(winning_array[i] + 10**j)
+			end
+		end
+
+
+		#breaking code for some reason
+
+		# for i in 0..(one_off.length - 1)
+		# 	if one_off[i] < 1000
+		# 		holder = one_off[i]
+		# 		one_off[i] = '0' + holder.to_s
+		# 	else
+		# 		holder = one_off[i]
+		# 		one_off[i] = holder.to_s
+		# 	end
+
+		# end
+
+		#puts one_off
+
 		for i in 0..(participants_array.length - 1)
-			if (winning_array.include?(participants_array[i]))
+			if (one_off.include?(participants_array[i]))
+				final_array2.push(participants_array[i])
+			end
+		end
+
+		#print final_array2
+
+		return final_array2
+	end
+end
+
+def losing_numbers_array(winning_array,participants_array)
+
+	winning_numbers = Array.new
+	final_array = Array.new
+
+	if winning_array.class != Array
+		return "Error, 1st entry must be an Array of numbers"
+	elsif participants_array.class != Array
+		return "Error, 2nd entry must be an Array of numbers"
+	else
+		winning_array.each do |x|
+	
+			element1 = x
+
+			if (x.class != Fixnum)
+				return "Error, array entries must be Fixed 4 digit numbers"
+			elsif ((element1 % 10000) != x )
+				return "Error, entries must be 4 digits"
+			end
+
+		end
+
+
+		participants_array.each do |x|
+	
+			element2 = x
+
+			if (x.class != Fixnum)
+				return "Error, array entries must be Fixed 4 digit numbers"
+			elsif ((element2 % 10000) != x )
+				return "Error, entries must be 4 digits"
+			end			
+
+		end
+
+
+
+		for i in 0..(winning_array.length - 1)
+			for j in 0..3
+				winning_numbers.push(winning_array[i] - 10**j)
+				winning_numbers.push(winning_array[i] + 10**j)
+			end
+			winning_numbers.push(winning_array[i])
+		end
+
+		#breaking code
+
+		# for i in 0..(winning_numbers.length - 1)
+		# 	if winning_numbers[i] < 1000
+		# 		holder = winning_numbers[i]
+		# 		winning_numbers[i] = '0' + holder.to_s
+		# 	else
+		# 		holder = winning_numbers[i]
+		# 		winning_numbers[i] = holder.to_s
+		# 	end
+
+		# end
+
+
+		#should be used after finding the matching numbers
+		# array_length = array.length
+		# array_length.times do |x|
+		# 	array[x] = array[x].to_s
+		# end
+
+		for i in 0..(participants_array.length - 1)
+			if (winning_numbers.include?(participants_array[i]) == false)
 				final_array.push(participants_array[i])
 			end
 		end
 
+		#puts possible_winning_numbers
 		#puts final_array
 
 		return final_array
--- test_lottery.rb
+++ test_lottery.rb
@@ -31,4 +31,35 @@ class TestLottery < Minitest::Test
 		assert_equal([1234,9999,5678],winning__numbers_array([1234,8888,7654,9999,4321,5678],[1234,4325,2345,9999,6789,5678]))
 	end
 
+	def test_1st_entry_not_array_returns_error_one_off
+		assert_equal("Error, 1st entry must be an Array of numbers",winning__numbers_array(12345,['1234','1235','1236','1234']))
+	end
+
+	def test_2nd_entry_not_array_returns_error_one_off
+		assert_equal("Error, 2nd entry must be an Array of numbers",winning__numbers_array(['1234','1235','1236','1234'],12345))
+	end
+
+	def test_not_4_digit_entries_in_array1_one_off
+		assert_equal("Error, entries must be 4 digits", winning__numbers_array([1234,12345,1234], [1234,1234,1234]))
+	end
+
+	def test_not_4_digit_entries_in_array2_one_off
+		assert_equal("Error, entries must be 4 digits", winning__numbers_array([1234,1234,1234], [1234,12345,1234]))
+	end
+
+	def test_array1_entries_not_fix_nums_returns_errors_one_off
+		assert_equal("Error, array entries must be Fixed 4 digit numbers", winning__numbers_array([1234, "Hello", '1234'], [1234,1234,1234]))
+	end
+
+	def test_array2_entries_not_fix_nums_returns_errors_one_off
+		assert_equal("Error, array entries must be Fixed 4 digit numbers", winning__numbers_array([1234,1234,1234], [1234, "Hello", '1234']))
+	end
+
+	def test_working_condition_one_off
+		assert_equal([1235,9998,5679],one_off_array([1234,8888,7654,9999,4321,5678],[1235,4325,2345,9998,6789,5679]))
+	end
+
+	def test_working_condition_losing_numbers
+		assert_equal([4325,2345,6789],losing_numbers_array([1234,8888,7654,9999,4321,5678],[1234,4325,2345,9999,6789,5678]))
+	end
 end
